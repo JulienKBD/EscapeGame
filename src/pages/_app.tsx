cea1a2e21ac465d039ac6204474cea92df09d462
JulienKBD <kbidi.julien.97418@gmail.com>
2025-10-30 03:13:40 +0400

    [ADD]: Init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
+import { Geist, Geist_Mono } from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function App({ Component, pageProps } : AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Escape Game</title>
+        <meta
+          name="description"
+          content="Escape Game | Nuits Impacts 2025."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={`${geistSans.className} ${geistMono.className} relative min-h-screen`}>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
 }
---
+++ src/pages/quizz.tsx
@@ -0,0 +1,8 @@
+
+export default function QuizzPage() {
+    return (
+        <div>
+            toto
+        </div>
+    );
+}
